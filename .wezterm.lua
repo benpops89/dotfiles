179f870b1304fc8358f07afd51b21ae7c9c6e414
Ben Poppy <me@benpoppy.dev>
2024-06-19 17:32:05 +0100

    (wezterm): simplify config and add mac settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,32 @@
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
+local wezterm = require("wezterm")
+local config = {
 
--- Do not show the tab bar
-config.enable_tab_bar = false
+	-- General settings
+	enable_tab_bar = false,
 
--- Set the opacity of the window
-config.window_background_opacity = 0.8
+	-- MacOS specific settings
+	native_macos_fullscreen_mode = false,
+	macos_window_background_blur = 20,
 
--- Define the color scheme to use
-config.color_scheme = 'Catppuccin Macchiato'
+	-- Window settings
+	window_background_opacity = 0.8,
+	window_decorations = "RESIZE",
+	adjust_window_size_when_changing_font_size = false,
 
--- Change shell to zsh
-config.default_prog = { '/usr/bin/zsh' }
+	-- Colour scheme
+	color_scheme = "Catppuccin Macchiato",
+
+	-- Fonts
+	font_size = 14.0,
+
+	-- Key mappings
+	keys = {
+		{
+			key = "Enter",
+			mods = "ALT",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+}
 
 return config
